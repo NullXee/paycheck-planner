f5c933f3c56844a330de1058fd913c9117606211
NullXee <nuggxt.dev@icloud.com>
2025-12-05 23:02:25 -0600

    Initial Commit

---
+++ bills-are-paid/bills_are_paidApp.swift
@@ -0,0 +1,17 @@
+//
+//  bills_are_paidApp.swift
+//  bills-are-paid
+//
+//  Created by NullDev on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct bills_are_paidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
