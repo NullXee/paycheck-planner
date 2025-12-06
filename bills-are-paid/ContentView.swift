896c46e86651783d3d8afec1a68cf51f3af9be7a
NullXee <nuggxt.dev@icloud.com>
2025-12-06 00:46:08 -0600

    Starting point. Working on setting up the UI and allowing users to make an input after hittong a button, Then having it display in the graph

--- bills-are-paid/ContentView.swift
+++ bills-are-paid/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
+// Sets the base value to 0 of each Incime and Expense.
+var monthlyIncome: Double = 0  // Total monthly income.
+var expenseTotal: Double = 0 // How much the expense is.
+var expenseName: String = ""  // Holds the name of the expense.
+var expenses: [Any] = []  // Array for all expenses text and total. [Any] will allow both strings and doubles in it.
+
+// Main content \\
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        // Label/Header
+        ZStack {
+            Label("Bills are paid", systemImage: "creditcard.circle")
+                .font(Font.largeTitle)
+                .bold(true)
+                .foregroundColor(Color.blue)
         }
         .padding()
+
+        // Add buttons
+        // @State var set to false
+        HStack {
+            Button(action: {
+                @State var showIncomeInput = false
+            }) {
+                Text("Add Income")
+            }
+            Button(action: {
+                @State var showExpenseInput = false
+            }) {
+                Text("Add Expense")
+
+            }
+        }
     }
 }
 
